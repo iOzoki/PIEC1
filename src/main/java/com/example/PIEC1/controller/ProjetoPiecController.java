8772a16163133911922d28966a139185677462f9
Isaac Adler <isaac.adler@ufrpe.br>
2025-02-04 12:51:33 -0300

    Repair MAV

--- src/main/java/com/example/PIEC1/controller/ProjetoPiecController.java
+++ src/main/java/com/example/PIEC1/controller/ProjetoPiecController.java
@@ -2,13 +2,12 @@ package com.example.PIEC1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProjetoPiecController {
 
+    //Consertar a misera do mapping
     @GetMapping("/home")
     public ModelAndView getHome() {
         ModelAndView view = new ModelAndView();
